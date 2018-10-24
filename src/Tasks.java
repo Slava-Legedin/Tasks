b897d650930d4858e7279b48f339d6919cab3cf1
Slava <44290297+Slava-Legedin@users.noreply.github.com>
2018-10-24 10:54:32 +0300

    TestTasks

--- src/Tasks.java
+++ src/Tasks.java
@@ -6,9 +6,11 @@ public class Tasks {
     private static double radius;
     private static double side;
     private static final double pi = 3.14;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
 
-        System.out.println(Calc.Test());
+        //System.out.println(Calc.Test());
+
+        getArea();
             /*
             System.out.println("Please enter length of the side: ");
 
@@ -16,13 +18,14 @@ public class Tasks {
 
             System.out.println("Please enter circle radius: ");
             System.out.println(circleArea());
-
+*/
             System.out.println("Is my Sqaure is the Circle? Answer: " +isSqInCircle());
             System.out.println("Is my Circle is the Sqaure? Answer: " +isCircleinSq());
-            */
+
         }
 
-        public static double sqaureArea(){
+        private static void getArea(){
+            System.out.println("Please enter length of the side: ");
             Scanner read = new Scanner(System.in);
             double sqArea;
             try {
@@ -31,9 +34,19 @@ public class Tasks {
                 System.out.println("Only digits are acceptable!");
             }
             sqArea=side*side;
-            return sqArea;
+            System.out.println(sqArea);
+
+            System.out.println("Please enter circle radius: ");
+            double crclArea;
+            try { radius = read.nextDouble();}
+            catch (Exception e) {
+                System.out.println("Only digits are acceptable!");
+            }
+            crclArea=pi*radius*radius;
+            System.out.println(crclArea);
+            //return sqArea;
         }
-        public static double circleArea() throws Exception{
+        /*public static double circleArea() throws Exception{
             Scanner read = new Scanner(System.in);
             double crclArea;
             try { radius = read.nextDouble();}
@@ -42,16 +55,12 @@ public class Tasks {
             }
             crclArea=pi*radius*radius;
             return crclArea;
+        }*/
+        private static boolean isSqInCircle(){
+            return Math.sqrt(2) * side < radius * 2;
         }
-        public static boolean isSqInCircle(){
-            if (Math.sqrt(2)*side < radius*2)
-                return true;
-                else return false;
-        }
-        public static boolean isCircleinSq(){
-            if (radius*2 < side)
-                return true;
-            else return false;
+        private static boolean isCircleinSq(){
+            return radius * 2 < side;
 
     }
     }
